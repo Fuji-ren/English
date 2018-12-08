276c414f9a7bbfba15eda05e40e88da4b6708dce
Fuji-ren <renfu.3731@gmail.com>
2018-12-08 13:32:35 +0900

    Initial Commit

---
+++ 電子英単語帳/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  電子英単語帳
+//
+//  Created by 藤井廉 on 2018/12/08.
+//  Copyright © 2018年 university. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
